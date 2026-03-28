fe8f500043b9e1f9e45170e7b7c991c165ad6093
Deepanshu Tewari <tewarideepanshu804@gmail.com>
2026-03-29 01:16:20 +0530

    EVALUATION PIPELINE

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from src.datascience.pipeline.data_validation_pipeline import DataValidationTrai
 
 from src.datascience.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 from src.datascience.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
-#from src.datascience.pipeline.model_evaluation_pipeline import ModelEvaluationTrainingPipeline
+from src.datascience.pipeline.model_evaluation_pipeline import ModelEvaluationTrainingPipeline
   
 
 STAGE_NAME = "Data Ingestion stage"
@@ -54,3 +54,15 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e        
+
+
+
+TAGE_NAME = "Model evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelEvaluationTrainingPipeline()
+   data_ingestion.initiate_model_evaluation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
